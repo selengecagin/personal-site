16199307e75e601d28199dea288d8e83de236740
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-04 21:10:31 +0300

    data-header created

---
+++ my-web-page/src/data.jsx
@@ -0,0 +1,8 @@
+const data = {
+  name: "Selenge",
+  header: "I am a Frontend Developer...",
+  description:
+    "...who likes to craft solid and scalable frontend products with great user experiences.",
+  buttons: ["Github", "Linkedin"],
+  imageURL: "https://picsum.photos/200",
+};
