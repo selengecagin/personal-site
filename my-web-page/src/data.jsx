8ee318abdfe5af92e6c14aa5342bad6381beaa2c
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-05 00:05:21 +0300

    data is created- further update might be necessary

--- my-web-page/src/data.jsx
+++ my-web-page/src/data.jsx
@@ -2,9 +2,33 @@ const data = {
   name: "Selenge",
   header: "I am a Frontend Developer...",
   description:
-    "...who likes to craft solid and scalable frontend products with great user experiences.",
+    "I am a Frontend Developer who likes to craft solid and scalable frontend products with great user experiences",
   buttons: ["Github", "Linkedin"],
   imageURL: "https://picsum.photos/200",
 
   skills: ["JAVASCRIPT", "REACT", "REDUX", "NODE", "REACT", "VS CODE", "FIGMA"],
+
+  profile: {
+    birthDate: "11.01.1998",
+    city: "İstanbul",
+    education:
+      "Boğaziçi Üniversitesi, Computer Education and Educational Technologies,2024",
+    preferredPosition: "Frontend, UI",
+  },
+
+  aboutMe: [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent accumsan, nisi in vehicula varius, eros leo fermentum orci, ac tincidunt ipsum magna eu nulla. Mauris ut molestie nisl, eget pulvinar tellus. Nulla tincidunt ornare massa, non pellentesque libero venenatis pretium. Morbi eu lectus semper, fringilla metus a, sagittis mauris. ",
+    "Phasellus tristique lacus neque, laoreet fermentum metus maximus luctus. Donec rutrum tellus vel augue tincidunt fringilla. Donec faucibus lacus in justo vulputate imperdiet. Proin tristique malesuada lectus sed semper. Suspendisse potenti. ",
+  ],
+  projects: ["Workintech", "Journey"],
+  projectTexts: [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In neque ante, lobortis vitae tincidunt in, volutpat quis lacus. Sed sollicitudin nisi elit, vel auctor est cursus non. Quisque eu efficitur felis. Cras nisl ligula, tincidunt nec nulla nec, ultrices fringilla nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac suscipit arcu. Nullam nec tellus vitae erat iaculis varius ut eu libero. Nam lectus massa, malesuada in dignissim eu, semper a ipsum. ",
+    "Cras quis tortor in velit porta ultricies sed ut arcu. Proin volutpat nisl sapien, eget elementum nibh imperdiet et. Maecenas mi mi, maximus quis velit et, eleifend hendrerit nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras vel elementum eros. Donec risus mauris, scelerisque et purus maximus, ultrices sodales nibh. Vestibulum aliquet, tortor eu sagittis varius, metus nulla finibus nulla, quis pulvinar diam dui vel nisl. ",
+  ],
+
+  contact: {
+    title: "Send me a message!",
+    text: "Got a question or proposal, or just want to say hello? Go ahead.",
+    email: "selengecagin@gmail.com",
+  },
 };
