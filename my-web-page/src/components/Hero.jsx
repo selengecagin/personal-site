30b91949e758acadb8c72a84120a296b418cb262
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-05 00:13:17 +0300

    hero component created

--- my-web-page/src/components/Hero.jsx
+++ my-web-page/src/components/Hero.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
 
 export default function Hero() {
   return (
     <div>
-      
+      <Header />
+      <h1>header</h1>
+      <p>description</p>
+      <img src="imgURL"></img>
+      <p>link-0</p>
+      <p>link-1</p>
     </div>
-  )
+  );
 }
