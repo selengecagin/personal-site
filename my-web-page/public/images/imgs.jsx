4b33d3b5a23aa1354c0bc9c885a8c77523a88ddd
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-05 23:03:37 +0300

    images added

---
+++ my-web-page/public/images/imgs.jsx
@@ -0,0 +1,2 @@
+//why we use a public folder to hold CSS and images folder?
+// https://stackoverflow.com/questions/63627376/can-anyone-explain-me-why-we-use-a-public-folder-to-hold-css-and-image-folder
