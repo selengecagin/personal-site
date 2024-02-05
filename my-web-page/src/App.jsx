76ef0f079557b04323692a718d2207eda1d78254
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-05 09:43:00 +0300

    components added to root

--- my-web-page/src/App.jsx
+++ my-web-page/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
-
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Profile from "./components/Profile";
+import Contact from "./components/Contact";
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Hero />
+      <Skills />
+      <Projects />
+      <Profile />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
