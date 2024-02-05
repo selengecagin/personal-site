848417def38af84a374a680ee9fcf7676a65af72
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-05 09:37:49 +0300

    profile component created

--- my-web-page/src/components/Profile.jsx
+++ my-web-page/src/components/Profile.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Profile() {
   return (
     <div>
-      
+      <div>
+        <h3>Basic Information</h3>
+        {/*info map */}
+      </div>
+      <div>
+        <img src="imgURL" alt="" />
+      </div>
+      <div>
+        <h3>About Me</h3>
+        {/* about me map */}
+      </div>
     </div>
-  )
+  );
 }
