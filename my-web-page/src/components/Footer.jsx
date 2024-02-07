c15b0c47355cd4ae123238b962126e9a7c8a6c11
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-07 05:23:01 +0300

    footer icons added- not completed

--- my-web-page/src/components/Footer.jsx
+++ my-web-page/src/components/Footer.jsx
@@ -1,15 +1,28 @@
 import React, { useContext } from "react";
 import { DataContext } from "../context/DataContext";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faCodepen,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
   const { data } = useContext(DataContext);
   const { footer } = data;
 
   return (
-    <div>
+    <div className="footer">
       <h3>{footer?.title}</h3>
       <p>{footer?.text}</p>
       <p>{footer?.email}</p>
+      <div className="icons">
+        <FontAwesomeIcon icon={faTwitter} />
+        <FontAwesomeIcon icon={faCodepen} />
+        <FontAwesomeIcon icon="fa-solid fa-at" />
+        <FontAwesomeIcon icon={faInstagram} />
+      </div>
     </div>
   );
 }
