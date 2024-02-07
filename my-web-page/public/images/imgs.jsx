32e3515ae758eac58f91d56f6da621572dccf008
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-07 03:26:34 +0300

    error handling

--- my-web-page/public/images/imgs.jsx
+++ my-web-page/public/images/imgs.jsx
@@ -1,2 +1,16 @@
 //why we use a public folder to hold CSS and images folder?
 // https://stackoverflow.com/questions/63627376/can-anyone-explain-me-why-we-use-a-public-folder-to-hold-css-and-image-folder
+
+
+
+/* //colors//
+ blue: #4731D3; yeşil:  #CBF281; pembe: 
+#E92577 , açık mor:#8F88FF, sarı: #FFE86E hero buttons: Default/indigo/800
+#3730A3
+Default/white
+#FFFFFF
+
+
+//Fonts//
+@import url("https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@100;200&family=Inter:wght@700&display=swap");
+*/
