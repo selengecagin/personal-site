24991f14635ffb7df6d5dd8f6e56579b4a3e91ce
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-08 10:08:57 +0300

    footer tailwind update

--- my-web-page/src/components/Footer.jsx
+++ my-web-page/src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { DataContext } from "../context/DataContext";
-import { ThemeContext } from "../context/ThemeContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faTwitter,
@@ -12,26 +11,20 @@ import { faAt } from "@fortawesome/free-solid-svg-icons";
 export default function Footer() {
   const { data } = useContext(DataContext);
   const { footer } = data;
-  // const { theme } = useContext(ThemeContext);
+
   return (
-    <div
-    // className={`flex justify-center py-20 ${
-    //   theme === "dark"
-    //     ? "bg-[#252128] text-[#8F88FF]"
-    //     : "bg-[#F9F9F9] text-[#4731D3]"
-    // }`}
-    >
+    <div className={`flex justify-center py-20`}>
       <div className="sm:w-1/3">
-        <h3 className="text-[48px] font-bold">{footer?.title}</h3>
-        <p
-          // className={`text-[30px] px-12 leading-10 pt-5 ${
-          //   theme === "dark" ? " text-[#FFFFFF]" : "text-[#120B39]"
-          // }`}
-        >
+        <h3 className="text-[48px] font-bold text-[#3730A3] text-center">
+          {footer?.title}
+        </h3>
+        <p className={`text-[30px] px-12 leading-10 pt-5 text-center`}>
           {footer?.text}
         </p>
-        <p className="pt-10 text-xl underline">{footer?.email}</p>
-        <div className="px-5 justify-center space-x-4 mt-4 ">
+        <p className="pt-10 text-xl underline text-[#3730A3] text-center">
+          {footer?.email}
+        </p>
+        <div className="px-5 justify-center space-x-4 mt-4 text-[#3730A3] text-center">
           <FontAwesomeIcon icon={faTwitter} className="text-2xl" />
           <FontAwesomeIcon icon={faCodepen} className="text-2xl" />
           <FontAwesomeIcon icon={faAt} className="text-2xl" />
