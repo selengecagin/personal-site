4d9129c80fc9f35ffda0a9009e1523432a5e1c33
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-08 13:57:54 +0300

    theme context start over

--- my-web-page/src/context/ThemeContext.jsx
+++ my-web-page/src/context/ThemeContext.jsx
@@ -3,17 +3,11 @@ import React, { createContext, useEffect, useState } from "react";
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark"
-  );
+  const [theme, setTheme] = useState("");
 
   const toggleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
-
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
