ae4f0969c26528e3f283bf816efaf38cff262d14
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-08 05:50:12 +0300

    header tailwind update

--- my-web-page/src/components/Header.jsx
+++ my-web-page/src/components/Header.jsx
@@ -4,5 +4,9 @@ import { DataContext } from "../context/DataContext";
 export default function Header() {
   const { data } = useContext(DataContext);
   const { name } = data;
-  return <div>{name}</div>;
+  return (
+    <div className="header">
+      <h3 className="text-[#CBF281] text-[2rem] mt-8 "> {name}</h3>
+    </div>
+  );
 }
