062044052e87476aa95e012e154914bb095477e7
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-08 06:27:58 +0300

    css and tailwind update

--- my-web-page/src/components/Footer.jsx
+++ my-web-page/src/components/Footer.jsx
@@ -12,25 +12,25 @@ import { faAt } from "@fortawesome/free-solid-svg-icons";
 export default function Footer() {
   const { data } = useContext(DataContext);
   const { footer } = data;
-  const { theme } = useContext(ThemeContext);
+  // const { theme } = useContext(ThemeContext);
   return (
     <div
-      className={`flex justify-center py-20 ${
-        theme === "dark"
-          ? "bg-[#252128] text-[#8F88FF]"
-          : "bg-[#F9F9F9] text-[#4731D3]"
-      }`}
+    // className={`flex justify-center py-20 ${
+    //   theme === "dark"
+    //     ? "bg-[#252128] text-[#8F88FF]"
+    //     : "bg-[#F9F9F9] text-[#4731D3]"
+    // }`}
     >
       <div className="sm:w-1/3">
-        <h3 className="text-[48px] font-bold">{contact.title}</h3>
+        <h3 className="text-[48px] font-bold">{footer?.title}</h3>
         <p
-          className={`text-[30px] px-12 leading-10 pt-5 ${
-            theme === "dark" ? " text-[#FFFFFF]" : "text-[#120B39]"
-          }`}
+          // className={`text-[30px] px-12 leading-10 pt-5 ${
+          //   theme === "dark" ? " text-[#FFFFFF]" : "text-[#120B39]"
+          // }`}
         >
-          {contact.text}
+          {footer?.text}
         </p>
-        <p className="pt-10 text-xl underline">{contact.email}</p>
+        <p className="pt-10 text-xl underline">{footer?.email}</p>
         <div className="px-5 justify-center space-x-4 mt-4 ">
           <FontAwesomeIcon icon={faTwitter} className="text-2xl" />
           <FontAwesomeIcon icon={faCodepen} className="text-2xl" />
--- my-web-page/src/components/Hero.jsx
+++ my-web-page/src/components/Hero.jsx
@@ -6,6 +6,7 @@ import { LanguageContext } from "../context/LanguageContext";
 import { dataEng, dataTr } from "../data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { ThemeContext } from "../context/ThemeContext";
 
 // run this command and import:
 // npm i --save @fortawesome/free-brands-svg-icons
@@ -25,7 +26,8 @@ const languageButton = () => {
 export default function Hero() {
   const { data, setData } = useContext(DataContext);
   const { header, description, heroIMG, buttons } = data;
-  const { language } = useContext(LanguageContext);
+  const { language, toggleLanguage } = useContext(LanguageContext);
+  // const { theme } = useContext(ThemeContext);
 
   useEffect(() => {
     const showLng = language === "en" ? dataEng : dataTr;
@@ -33,53 +35,70 @@ export default function Hero() {
   }, [language, setData]);
 
   return (
-    <div className="hero">
-      <Header />
-      {languageButton()}
-      <div className="hero-container">
-        <div className="hero-content-left">
-          <h1>{header}</h1>
-          <p className="pTag">{description}</p>
-
-          <div className="hero-btn">
-            {buttons?.map((button, index) => {
-              return (
-                <div className="btn" key={index}>
-                  <button>
-                    <FontAwesomeIcon
-                      icon={index % 2 === 0 ? faGithub : faLinkedin}
-                      size="2x"
-                    />
-                    <p>{button}</p>
-                  </button>
-                </div>
-              );
-            })}
+    <div
+      // className={`hero-container flex flex-wrap-reverse sm:no-wrap justify-center m-auto ${
+      //   theme === "dark" ? "bg-[#171043]" : "bg-[#4731D3]"
+      // }`}
+    >
+      <div className="sm:w-2/3 sm:pl-[15%] sm:pb-20 sm:pt-12 sm:relative ">
+        <div className="hero-header flex flex-col justify-center h-auto ">
+          <div className="header-right  flex flex-row items-end  content-center justify-around sm:justify-end sm:pl-[55%] sm:pr-[5%]">
+            (languageButton())
+          </div>
+          <div className="header-left flex justify-center sm:justify-start">
+            <Header />
           </div>
         </div>
-        <div className="hero-img-right">
-          <img src={heroIMG} alt="Hero Image"></img>
+        <div className="hero-container flex flex-col py-[0] justify-center m-auto">
+          <div className="flex flex-wrap-reverse sm:flex-nowrap sm:flex-row ">
+            <div className="hero-left mb-8 md:mb-0 md:w-1/2 ">
+              <h1 className="font-bold text-[3rem] leading-[59.4px] text-[#CBF281] mx-12 sm:ml-0 sm:mt-20 text-left mb-8">
+                {header}
+              </h1>
+              <p className="font-normal text-[24px] mx-12 sm:ml-0 leading-[30px] text-[#FFFFFF] text-left">
+                {description}
+              </p>
+              <div className="hero-btn flex items-center justify-center sm:justify-start mt-6 flex-row gap-2 md:space-x-4">
+                {buttons &&
+                  buttons.map((btn, index) => (
+                    <div key={index}>
+                      <button
+                        // className={`flex w-[127px] h-[52px] rounded-[6px] border-[1px] border-[solid] pl-[12px] pr-[20px,] py-[12px,] text-[#3730A3] flex-row items-center justify-evenly font-medium text-[18px] leading-[28px] mt-8 ${
+                        //   theme === "dark"
+                        //     ? "bg-[#252128] text-[#FFFFFF]"
+                        //     : "bg-[#FFFFFF] text-[#3730A3]"
+                        // }`}
+                      >
+                        {index % 2 === 0 ? (
+                          <FontAwesomeIcon icon={faGithub} size="2x" />
+                        ) : (
+                          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                        )}
+                        <p
+                          // className={`text-[1rem] ${
+                          //   theme === "dark"
+                          //     ? "text-[#FFFFFF]"
+                          //     : "text-[#3730A3]"
+                          // }`}
+                        >
+                          {btn}
+                        </p>
+                      </button>
+                    </div>
+                  ))}
+              </div>
+            </div>
+            <img
+              className="hero-right max-w-full h-auto sm:absolute top-[100px] -right-[260px]"
+              src={heroIMG}
+              alt="woman-working-laptop"
+            />
+          </div>
         </div>
+      </div>
+      <div className="sm:bg-[#CBF281] sm:w-1/3 sm:pr-[20%] sm:pb-20  mb-[10px] sm:mb-0  sm:pt-6">
+        {/* Theme button */}
       </div>
     </div>
   );
-}
-
-// notes:
-//  add key={index} to avoid "Each child in a list should have a unique 'key' prop"
-//  while adding key={index} may suppress the warning, it's best to ensure that each item in the list has a
-//  stable and unique identifier to improve the performance and correctness of your React components.
-//  Do my solution provide this??
-
-{
-  /* <div className="btn" key={index}>
-  <button>
-    {index % 2 === 0 ? (
-      <FontAwesomeIcon icon={faGithub} size="2x" />
-    ) : (
-      <FontAwesomeIcon icon={faLinkedin} size="2x" />
-    )}
-    <p>{button}</p>
-  </button>
-</div>; */
 }
