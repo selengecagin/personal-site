63a000aff913307b25dbe4d2f60228d250ae0e7b
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-02-08 13:53:32 +0300

    language context updated with uselocalStorage hook

--- my-web-page/src/context/LanguageContext.jsx
+++ my-web-page/src/context/LanguageContext.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { createContext } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 export const LanguageContext = createContext();
 
 export default function LanguageContextProvider({ children }) {
-  const [language, setLanguage] = useState("en");
+  const [language, setLanguage] = useLocalStorage("language", "en");
 
   const toggleLanguage = () => {
     language === "en" ? setLanguage("tr") : setLanguage("en");
